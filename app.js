0ca3601a1f169877126e03fa29d274b50d4269f8
DRHood <darryl.r.hood@gmail.com>
2020-02-03 13:26:01 -0500

    getting things set up

--- app.js
+++ app.js
@@ -1,3 +1,24 @@
-console.log("Hello from app.js")
+let chAmount = document.querySelector('#chAmnt').value;
+let chDeposit = document.querySelector('#chDep');
+let chWithdraw = document.querySelector('#chWdrw');
+let saAmount = document.querySelector('#saAmnt').value;
+let saDeposit =document.querySelector('#saDep');
+let saWithdraw = document.querySelector('#saWdrw');
 
-document.body.style.background = "red";
+submitOK = 'true';
+
+// Checking Account
+// deposit
+// event listener attached to deposit button
+document.querySelector('#chDep').addEventListener('click', depositChecking);
+// get amount from input field
+// store in variable
+// add amount to account total
+
+// withdraw
+// get amount from input field
+
+// subtract amount from account total
+
+
+// Savings Account
