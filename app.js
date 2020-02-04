6b34119c76238dec03979725265ee40c4a31132c
DRHood <darryl.r.hood@gmail.com>
2020-02-03 23:29:05 -0500

    balences cannot go below 0, alert for insufficient funds

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // Global sum variables
 chBalNum = 0;
 saBalNum = 0;
+
 // Checking Account
 // deposit 
 // event listener attached to deposit button
@@ -10,42 +11,67 @@ document.querySelector('#chDep').addEventListener('click', () => {
     // change input amount to integer
     let chNumber = parseInt(chAmount);
     // get checking balance
-    let chBalance = document.querySelector('#chBal').textContent;
+    let chBalance = document.querySelector('#chBal');
     // add input and balance
     chBalNum += chNumber;
     // display new balance
-    document.querySelector('#chBal').innerHTML = "$" + chBalNum;
+    chBalance.innerHTML = "$" + chBalNum;
     return chBalNum;
 });
 
-// Checking Account
 // withdrawal 
-// event listener attached to deposit button
+// event listener attached to withdraw button
 document.querySelector('#chWdrw').addEventListener('click', () => {
     // get amount from input field
     let chAmount = document.querySelector('#chAmnt').value;
     // change input amount to integer
     let chNumber = parseInt(chAmount);
     // get checking balance
-    let chBalance = document.querySelector('#chBal').textContent;
-    // add input and balance
-    chBalNum -= chNumber;
-    // display new balance
-    document.querySelector('#chBal').innerHTML = "$" + chBalNum;
+    let chBalance = document.querySelector('#chBal');
+    // subtract input from balance if available, otherwise alert
+    if(chNumber > chBalNum) {
+        alert("Insufficient Funds")
+    } else {
+        chBalNum -= chNumber;
+        // display new balance
+        chBalance.innerHTML = "$" + chBalNum;
+    }
     return chBalNum;
 });
 
+// Savings Account
+// deposit
 // event listener attached to deposit button
 document.querySelector('#saDep').addEventListener('click', () => {
     // get amount from input field
     let saAmount = document.querySelector('#saAmnt').value;
     // change input amount to integer
     let saNumber = parseInt(saAmount);
-    // get checking balance
-    let saBalance = document.querySelector('#saBal').textContent;
+    // get savings balance
+    let saBalance = document.querySelector('#saBal');
     // add input and balance
     saBalNum += saNumber;
     // display new balance
-    document.querySelector('#saBal').innerHTML = "$" + saBalNum;
+    saBalance.innerHTML = "$" + saBalNum;
+    return saBalNum;
+});
+
+// withdrawal
+// event listener attached to withdraw button
+document.querySelector('#saWdrw').addEventListener('click', () => {
+    // get amount from input field
+    let saAmount = document.querySelector('#saAmnt').value;
+    // change input amount to integer
+    let saNumber = parseInt(saAmount);
+    // get savings balance
+    let saBalance = document.querySelector('#saBal');
+    // subtract input from balance if available, otherwise alert
+    if(chNumber > chBalNum) {
+        alert("Insufficient Funds")
+    } else {
+        chBalNum -= chNumber;
+        // display new balance
+        chBalance.innerHTML = "$" + chBalNum;
+    }
     return saBalNum;
 });
